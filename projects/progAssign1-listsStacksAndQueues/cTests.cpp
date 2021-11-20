21d5c4bd98d54df7a2f789b797e5216cd79e9c49
Neil Sawhney <Neil@neil-sawhney.com>
2021-11-19 19:42:08 -0500

    got up to pointers

--- projects/progAssign1-listsStacksAndQueues/cTests.cpp
+++ projects/progAssign1-listsStacksAndQueues/cTests.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+// template stuff
+template <typename Data>
+void print(Data dog) { cout << dog; }
+
+
+//pointer stuff
+//taking in the address so that we can edit the actual variable
+// a pointer is a data type that holds an address (int *).  A reference is a data type that holds an address like a pointer, but spits it the value when you remove the &.  The address is accessed with &variable, the value is accesed with variable
+void pointer(int &fingie)
+{
+ fingie++; 
+}
+
 int main() {
 
   /*
@@ -16,26 +29,50 @@ int main() {
 
   /*
    cout << "you've encountered a bear, give it a file\n";
-
    // ask the user for a file name
    string inputFile;
    cin >> inputFile;
    ifstream input(inputFile);
    */
 
-  // print every line in file "commands1.txt" and paste into a new file called "cTestsOutput.txt"
-
+  /*
+  // print every line in file "commands1.txt" and paste into a new file
+  string s;
   ifstream input("commands1.txt");
   ofstream output("cTestsOutput.txt");
-
-  string s;
   while (getline(input, s)) {
     cout << s << "\n";
     output << s << "\n";
   }
+
+  //close the files
   input.close();
   output.close();
+*/
 
+/*
+  //printing template stuff 
+  int cat = 5;
+  string bat = "kacka";
+  print(cat);
+  print(bat);
+*/
+
+  /*
+  //pointer stuff
+  int cat = 5;
+  int *bat = &cat;
+  int &kangaroo = cat;
+  print(cat);
+  print(kangaroo);
+  cout << "\n";
+  print(bat);
+  cout << "\n";
+  //pass to pointer to increment it then print again
+  pointer(cat);
+  print(cat);
+  print(kangaroo);
+  */
 
   return 0;
 }
