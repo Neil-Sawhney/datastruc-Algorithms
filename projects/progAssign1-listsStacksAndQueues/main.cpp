28e42cfc23cfc11404822dc4003f4552b7798c75
Neil <neil@neil-sawhney.com>
2021-11-22 19:17:21 -0500

    SimpleList is donegit add ./! :):):) | time to work on the processing and printing!

--- projects/progAssign1-listsStacksAndQueues/main.cpp
+++ projects/progAssign1-listsStacksAndQueues/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <map>
+#include <new>
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
@@ -13,25 +17,128 @@ private:
 
     // constructor for Node accepts data and pointer, if no pointer is provided
     // it is defaulted to NULL
-    Node(Data DATA, Node *NEXT = NULL) {
+    Node(Data DATA, Node *NEXT = nullptr) {
       data = DATA;
       next = NEXT;
     }
   };
 
 private:
-  Node *first; // first node of the list
-  Node *last;  // last node of the list
+  // pointers to start and end of list
+  Node *first = nullptr;
+  Node *last = nullptr;
+
 public:
-  int size; // size of the list
-  SimpleList() {
-    first, last, size = NULL, NULL, 0;
-  } // constructor for SimpleList
+  int size = 0;
+  string listName;
+
+  SimpleList(string LISTNAME = NULL) { this->listName = LISTNAME; }
+
+  // function declaration and implementation beacause it's the same for both
+  // stacks and queues
+  Data pop() { return removeFromStart(); };
+  virtual Data push(Data DATA) = 0;
+
+  // a simple map to make stream puts and error catching cleaner to implement
+  static map<string, string> msg;
+
 protected:
-  virtual void push(Data DATA) = 0; // pure virtual function declarations to insert node at start or end, must be overridden by stacks and queues
-  Data pop(); // function declaration to remove node from the start, used for both stacks and queues
+  // functions that manipulate nodes to be used for push and pop
+  Data insertAtStart(Data DATA);
+  Data insertAtEnd(Data DATA);
+  Data removeFromStart();
 };
 
-int main(){
 
+  template<typename Data> map<string,string> SimpleList<Data>::msg = {
+      {"VAL_POP", "Value popped: \n"},
+      {"ERROR_EMPTY", "ERROR: This list is empty!\n"},
+      {"ERROR_DNE", "ERROR: This name does not exist!\n"},
+  };
+
+// A Templated Function that inserts a node at the start of the list
+// returns the value pushed
+template <typename Data> Data SimpleList<Data>::insertAtStart(Data DATA) {
+  // point our new node to the lists previous first node
+  Node *NEXT = first;
+  Node *node = new Node(DATA, NEXT);
+  // tell SimpleList that the lists first node is now our new node
+  first = node;
+  if (size == 0)
+    last = first;
+  size++;
+  return (DATA);
+}
+
+// A Templated Function that inserts a node at the end of the list
+// returns the value popped
+template <typename Data> Data SimpleList<Data>::insertAtEnd(Data DATA) {
+  if (size == 0)
+    insertAtStart(DATA);
+  else {
+    // point the lists last node to our new node
+    Node *node = new Node(DATA);
+    last->next = node;
+    // tell SimpleList that the lists last node is now our new node
+    last = node;
+    size++;
+  }
+  return (DATA);
+}
+
+// A Templated Function that removes a node at the start of the list
+// returns the data removed, or NULL if passed an empty list
+template <typename Data> Data SimpleList<Data>::removeFromStart() {
+  Node *tmp = first;
+  // empty list
+  if (size == 0)
+    throw invalid_argument("ERROR_EMPTY");
+  // tell SimpleList that the list's first node is the second node
+  first = first->next;
+  Data removedData = tmp->data;
+  delete tmp;
+  size--;
+  return removedData;
+}
+
+// A Templated Stack Class That Inherits SimpleList
+// Pushes to start and pops from start
+template <typename Data> class Stack : public SimpleList<Data> {
+public:
+  using SimpleList<Data>::SimpleList;
+  Data push(Data DATA) { return this->insertAtStart(DATA); };
+};
+
+// A Templated Queue Class That Inherits SimpleList
+// Pushes to end and pops from start
+template <typename Data> class Queue : public SimpleList<Data> {
+public:
+  using SimpleList<Data>::SimpleList;
+  Data push(Data DATA) { return this->insertAtEnd(DATA); };
+};
+
+
+
+void handler() {
+  SimpleList<int> *L1 = new Queue<int>("L1");
+
+  try {
+    cout << "pushed: " << L1->push(1) << "\n";
+    cout << "pushed: " << L1->push(2) << "\n";
+    cout << "pushed: " << L1->push(3) << "\n";
+    cout << "pushed: " << L1->push(4) << "\n";
+
+    cout << "popped: " << L1->pop() << "\n";
+    cout << "popped: " << L1->pop() << "\n";
+    cout << "popped: " << L1->pop() << "\n";
+    cout << "popped: " << L1->pop() << "\n";
+    cout << "popped: " << L1->pop() << "\n";
+  } catch (const invalid_argument &e) {
+    cout << SimpleList<int>::msg[e.what()]; 
+  }
+}
+
+int main(){
+    // dont mind me just testing some stuff!
+    handler();
 };
